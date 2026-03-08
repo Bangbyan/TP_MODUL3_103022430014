18a6605c327b89b63c12457939691f22bf3ec80d
Muhammad Abyan Efffendi <mhmdabyan19@gmail.com>
2026-03-09 00:15:39 +0700

    Menambahkan tampilan GUI kalkulator

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace TP_MODUL3_103022430014
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
